cdf25d3f21a27f2efe114c32fd649ca02e3878d6
nguynking <kingnguyen0123@gmail.com>
2024-05-25 11:30:06 +0700

    fix: using pydantic to ensure input type

--- app/api/main.py
+++ app/api/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import pipeline
 
 app = FastAPI(title="Extractive Question-Answering API", description="API to answer a question using provided context")
 
-# Load the model using Hugging Face's pipeline
 model_name = "quocviethere/xlmroberta-finetuned-squadv2"
 qa_pipeline = pipeline('question-answering', model=model_name)
 
-@app.post("/answer", summary="Answer a new question give context")
-def answer_question(context: str, question: str) -> dict:
-    answer = qa_pipeline(context=context, question=question)['answer']
+class QARequest(BaseModel):
+    context: str
+    question: str
+
+@app.post("/answer", summary="Answer a new question given context")
+def answer_question(request: QARequest) -> dict:
+    answer = qa_pipeline(context=request.context, question=request.question)['answer']
     return {"answer": answer}
--- app/frontend/app.py
+++ app/frontend/app.py
@@ -1,38 +1,28 @@
 import gradio as gr
 import requests
 
-# def classify_text(text):
-#     response = requests.post("http://api:8000/classify/", json={"text": text})
-#     return response.json()['classification']
-
-# Define a function that the interface will use
 def answer_question(context, question):
-    response = requests.post("http://api:8000/answer", json={"context": context, "question": question})
-    return response.json()['answer']
+    response = requests.post(
+        "http://api:8000/answer", 
+        json={"context": context, "question": question}
+    )
+    return response.json().get('answer', 'No answer returned')
 
-# iface = gr.Interface(
-#     fn=classify_text,
-#     inputs=gr.Textbox(lines=2, placeholder="Enter text here..."),
-#     outputs=gr.Label(num_top_classes=1),
-#     title="Text Classification",
-#     description="Classify text as Positive, Negative, or Neutral"
-# )
-
-# Define a single example
 examples = [
     ["Tính đến tháng 6/2023, Vườn ươm Viện Đổi mới sáng tạo đã ươm tạo gần 100 dự án khởi nghiệp, trong đó có 19 dự án thành lập công ty, hơn 7600 người tham dự các hoạt động và tổng giá trị đầu tư mạo hiểm đạt 350.000 USD.", 
      "Tổng giá trị đầu tư mạo hiểm của Vườn ươm Viện Đổi mới sáng tạo là bao nhiêu?"]
 ]
 
-# Create the Gradio interface
-iface = gr.Interface(fn=answer_question,
-                     inputs=[
-                         gr.Textbox(lines=7, placeholder="Enter the context here...", label="Context"),
-                         gr.Textbox(placeholder="Enter your question here...", label="Question")
-                     ],
-                     outputs=gr.Textbox(label="Answer"),
-                     title='UEH-QA',
-                     description='Creating safe AGI that benefits all of humanity.',
-                     examples=examples)
+iface = gr.Interface(
+    fn=answer_question,
+    inputs=[
+        gr.Textbox(lines=7, placeholder="Enter the context here...", label="Context"),
+        gr.Textbox(placeholder="Enter your question here...", label="Question")
+    ],
+    outputs=gr.Textbox(label="Answer"),
+    title='UEH-QA',
+    description='Creating safe AGI that benefits all of humanity.',
+    examples=examples
+)
 
 iface.launch(server_name="0.0.0.0", server_port=7860)
